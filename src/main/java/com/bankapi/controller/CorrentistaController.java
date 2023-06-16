07de30d0bd81c0f5ca662a9513311df7c0d2d421
vhraposo <thevhup@gmail.com>
2023-06-16 15:30:42 -0300

    add cross origin at localhost4200

--- src/main/java/com/bankapi/controller/CorrentistaController.java
+++ src/main/java/com/bankapi/controller/CorrentistaController.java
@@ -3,6 +3,7 @@ package com.bankapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/correntistas")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CorrentistaController {
     
     @Autowired

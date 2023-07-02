ae5d972932580bf393070efd054c9f529a73df05
Victor Raposo <thevhup@gmail.com>
2023-07-02 17:59:00 -0300

    add jwt at post login

--- src/main/java/com/bankapi/controller/UsuarioController.java
+++ src/main/java/com/bankapi/controller/UsuarioController.java
@@ -1,6 +1,11 @@
 package com.bankapi.controller;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,22 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bankapi.model.Usuario;
 import com.bankapi.repository.UsuarioRepository;
-
+import com.bankapi.utils.JwtUtil;
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
-    
+
     @Autowired
     private UsuarioRepository repo;
 
-   @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody Usuario userData) {
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> loginUser(@RequestBody Usuario userData) {
     Usuario usuario = repo.findByUserId(userData.getUserId());
-    if (usuario.getPassword().equals(userData.getPassword())) {
-        return ResponseEntity.ok(usuario);
+
+    if (usuario != null && usuario.getPassword().equals(userData.getPassword())) {
+        String userId = usuario.getUserId();
+        String token = JwtUtil.generateToken(userId);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+
+        return ResponseEntity.ok(response);
     }
-    return (ResponseEntity<?>) ResponseEntity.internalServerError();
+
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.singletonMap("error", "Credenciais inválidas"));
     }
+
 }
---
+++ src/main/java/com/bankapi/utils/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.bankapi.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 86400000; // 24 horas
+
+    public static String generateToken(String userId) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static String getUserIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
